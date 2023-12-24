14128e662a24bc9959ccc7c7e9f32aed393c036c
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2023-12-24 15:29:14 -0600

    init unity and xcode projects and integration workspace

---
+++ XcodeProject/UnitySwiftUIDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnitySwiftUIDemo
+//
+//  Created by Benjamin Dewey on 12/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeProject/UnitySwiftUIDemo/UnitySwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitySwiftUIDemoApp.swift
+//  UnitySwiftUIDemo
+//
+//  Created by Benjamin Dewey on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitySwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
