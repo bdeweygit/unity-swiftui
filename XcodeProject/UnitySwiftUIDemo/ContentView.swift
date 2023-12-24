fa66d7103609ef76025b0d27a19c8b6cfdeabbaf
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2023-12-24 17:19:08 -0600

    add UnityView and embed it within ContentView

--- XcodeProject/UnitySwiftUIDemo/ContentView.swift
+++ XcodeProject/UnitySwiftUIDemo/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            UnityView().ignoresSafeArea()
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
