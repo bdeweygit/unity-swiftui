1bbc17ff17f688667ce49bc1426df9a118afa7e2
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2023-12-28 15:36:39 -0600

    update comments; add examples of resizing the unity view

--- XcodeProject/UnitySwiftUIDemo/ContentView.swift
+++ XcodeProject/UnitySwiftUIDemo/ContentView.swift
@@ -8,16 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fullscreen = true
+    @State private var ignoreSafeArea = true
+    @State private var miniplayerSquare = true
+    @State private var miniplayerAlignment = Alignment.top
+
     var body: some View {
-        ZStack {
-            UnityView().ignoresSafeArea()
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+        GeometryReader { geometry in
+            ZStack {
+                ZStack(alignment: miniplayerAlignment) {
+                    Color.clear
+                    UnityView().if(fullscreen && ignoreSafeArea, transform: { view in
+                        view.ignoresSafeArea()
+                    }).if(!fullscreen && miniplayerSquare, transform: { view in
+                        view.frame(width: 200, height: 200)
+                    }).if(!fullscreen && !miniplayerSquare, transform: { view in
+                        view.frame(width: geometry.size.width * 0.5, height: geometry.size.height * 0.5)
+                    })
+                }
+                VStack {
+                    Picker("Player size", selection: $fullscreen) {
+                        Text("Fullscreen").tag(true)
+                        Text("Miniplayer").tag(false)
+                    }.pickerStyle(.segmented)
+                    if fullscreen {
+                        Picker("Fullscreen fill", selection: $ignoreSafeArea) {
+                            Text("Full area").tag(true)
+                            Text("Safe area").tag(false)
+                        }.pickerStyle(.segmented)
+                    } else {
+                        Picker("Miniplayer alignment", selection: $miniplayerAlignment) {
+                            Text("Top").tag(Alignment.top)
+                            Text("Center").tag(Alignment.center)
+                            Text("Bottom").tag(Alignment.bottom)
+                        }.pickerStyle(.segmented)
+                        Picker("Miniplayer shape", selection: $miniplayerSquare) {
+                            Text("Square").tag(true)
+                            Text("Aspect").tag(false)
+                        }.pickerStyle(.segmented)
+                    }
+                }.padding()
             }
-            .padding()
+        }
+    }
+}
+
+extension Alignment: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        switch self {
+        case .top: hasher.combine(0)
+        case .center: hasher.combine(1)
+        case .bottom: hasher.combine(2)
+        default: hasher.combine(3) // Handle custom alignments if needed
+        }
+    }
+}
+
+extension View {
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
         }
     }
 }
--- XcodeProject/UnitySwiftUIDemo/Unity.swift
+++ XcodeProject/UnitySwiftUIDemo/Unity.swift
@@ -8,49 +8,51 @@
 import UnityFramework
 
 class Unity: SetsNativeState {
-    static let shared = Unity() // singleton init is lazy and thread safe
+    static let shared = Unity() // Singleton init is lazy and thread safe
     private let framework: UnityFramework
     private var observation: NSKeyValueObservation?
 
     private init() {
-        // load unity framework
+        // Load unity framework
         let bundle = Bundle(path: "\(Bundle.main.bundlePath)/Frameworks/UnityFramework.framework")!
         bundle.load()
         self.framework = bundle.principalClass!.getInstance()!
 
-        // set header for unity CrashReporter - is this needed?
+        // Set header for unity CrashReporter; is this needed?
         let machineHeader = UnsafeMutablePointer<MachHeader>.allocate(capacity: 1)
         machineHeader.pointee = _mh_execute_header
         self.framework.setExecuteHeader(machineHeader)
 
-        // set bundle containing unity's data folder
+        // Set bundle containing unity's data folder
         self.framework.setDataBundleId("com.unity3d.framework")
 
-        // register as the native state setter
-        // note that Thread Performance Checker has been disabled in the UnitySwiftUIDemo scheme or else the mere presence, not the execution, of this line will cause a crash when running via Xcode
-        // see forum.unity.com/threads/unity-2021-3-6f1-xcode-14-ios-16-problem-unityframework-crash-before-main.1338284/
+        // Register as the native state setter
+        /* Note that we have disabled the Thread Performance Checker in the UnitySwiftUIDemo scheme
+         or else the mere presence of this line will instigate a crash before our code even executes
+         when running via Xcode. The Unity-iPhone scheme also has the Thread Performance Checker
+         disabled by default, perhaps for the same reason. See forum discussion: forum.unity.com/threads/unity-2021-3-6f1-xcode-14-ios-16-problem-unityframework-crash-before-main.1338284/ */
         RegisterNativeStateSetter(self)
 
-        // start the player; runEmbedded also calls framework.showUnityWindow internally
+        // Start the player; runEmbedded also calls framework.showUnityWindow internally
         self.framework.runEmbedded(withArgc: CommandLine.argc, argv: CommandLine.unsafeArgv, appLaunchOpts: nil)
 
-        // unity claims the key window, so let user interactions passthrough to our window
+        // Unity claims the key window so let user interactions passthrough to our window
         self.framework.appController().window.isUserInteractionEnabled = false
     }
 
     var superview: UIView? {
         didSet {
-            // remove old observation
+            // Remove old observation
             observation?.invalidate()
 
             if superview == nil {
                 self.framework.appController().window.rootViewController?.view.removeFromSuperview()
             } else {
-                // register new observation; it fires on register and on new value at .rootViewController
+                // Register new observation; it fires on register and on new value at .rootViewController
                 observation = self.framework.appController().window.observe(\.rootViewController, options: [.initial], changeHandler: { [weak self] (window, _) in
+                    /* The rootViewController of Unity's window has just been assigned
+                     so now is the proper moment to apply our superview if we have one */
                     if let superview = self?.superview, let view = window.rootViewController?.view {
-                        // the rootViewController of Unity's window has been assigned
-                        // now is the proper moment to apply our superview if we have one
                         superview.addSubview(view)
                         view.frame = superview.frame
                     }
@@ -59,7 +61,7 @@ class Unity: SetsNativeState {
         }
     }
 
-    // will point to a C# function in unity once a script calls
-    // the NativeState plugin's _OnSetNativeState function
+    /* This will point to a C# function in unity once a script calls
+     the NativeState plugin's _OnSetNativeState function */
     var setNativeState: SetNativeStateCallback?
 }
