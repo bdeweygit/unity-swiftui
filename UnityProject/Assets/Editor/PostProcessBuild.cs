24716d130cb54d652743c0f82a8f142a325c9721
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2024-07-09 19:34:55 -0500

    add comments

--- UnityProject/Assets/Editor/PostProcessBuild.cs
+++ UnityProject/Assets/Editor/PostProcessBuild.cs
@@ -1,12 +1,15 @@
 using UnityEditor;
 using UnityEditor.iOS.Xcode;
 
+/* We could implement UnityEditor.Build.IPostprocessBuildWithReport interface
+   but documentation is unclear if this is preferred over the below callback attribute.
+   There is a known bug with the interface's BuildReport having wrong information, so we opt to use the attribute. */
 public class PostProcessBuild
 {
     [UnityEditor.Callbacks.PostProcessBuildAttribute]
     public static void OnPostprocessBuild(BuildTarget target, string buildPath)
     {
-        /* Edit Unity Xcode project to enable Unity as a library:
+        /* Edit Unity generated Xcode project to enable Unity as a library:
            github.com/Unity-Technologies/uaal-example/blob/master/docs/ios.md */
         if (target == BuildTarget.iOS)
         {
@@ -28,7 +31,9 @@ public class PostProcessBuild
             project.RemoveFileFromBuild(unityMainTargetGuid, dataDirectoryGuid);
             project.AddFileToBuild(unityFrameworkTargetGuid, dataDirectoryGuid);
 
-            // Add custom modulemap for NativeState plugin interop with Swift
+            /* Add custom modulemap for NativeState plugin
+               interop with Swift and set corresponding build setting:
+               developer.apple.com/documentation/xcode/build-settings-reference#Module-Map-File */
             string modulemapRelativePath = "UnityFramework/UnityFramework.modulemap";
             string modulemapAbsolutePath = $"{buildPath}/{modulemapRelativePath}";
             FileUtil.ReplaceFile("Assets/Plugins/iOS/UnityFramework.modulemap", modulemapAbsolutePath);
